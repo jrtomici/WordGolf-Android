a3907fa79a614446bd8453873033a2c714530448
jrtomici <jrtomici@gmail.com>
2017-12-11 18:42:49 -0500

    Finishes contest activity once okay is clicked

--- app/src/main/java/com/bignerdranch/android/wordgolf/ContestActivity.java
+++ app/src/main/java/com/bignerdranch/android/wordgolf/ContestActivity.java
@@ -25,6 +25,7 @@ public class ContestActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(ContestActivity.this, SetupActivity.class));
+                finish();
             }
         });
 
